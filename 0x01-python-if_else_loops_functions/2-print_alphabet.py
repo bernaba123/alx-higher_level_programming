e72b07097bdfc57505ee82ca81b3dca5273dac15
bernaba123 <bernabasgetachew0946@gmail.com>
2021-04-27 10:02:44 +0000

    all

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i == 'e' or i == 'q':
-        continue
-    print("{:c}".format(i), end = "")
+for letter_code in range(ord('a'), ord('z')+1):
+    letter = chr(letter_code)
+    print(letter, end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i == ord('e') or i == ord('q'):
-        continue
-    print("{:c}".format(i), end = "")
+for letter_code in range(ord('a'), ord('z')+1):
+    letter = chr(letter_code)
+    if letter not in "qe":
+        print(letter, end="")
