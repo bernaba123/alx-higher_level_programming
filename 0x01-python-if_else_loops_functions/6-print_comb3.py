3a1e387a6d299637b35d2097ae69b25e2e18cb39
bernaba123 <bernabasgetachew0946@gmail.com>
2021-04-27 11:57:45 +0000

    all

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(1,10):
+    for j in range(1, 10):
         if j > i:
             if j != 9 or i != 8:
                 print("{:d}{:d}, ".format(i, j), end="")
