d6b88e19086c24f7e198fc0374ee08d968d2cc37
bernaba123 <bernabasgetachew0946@gmail.com>
2021-04-27 10:07:03 +0000

    all

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    print(letter, end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i == ord('e') and i == ord('q'):
+        continue
+    print("{:c}".format(i), end = "")
