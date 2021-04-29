787b9be23478ed7b1fc12b7edf7ad263ef3ebd77
bernaba123 <bernabasgetachew0946@gmail.com>
2021-04-29 14:12:56 +0000

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-elif len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-    print("{:d}: {:s}".format(len(sys.argv), sys.argv[1]))
-else:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {:s}".format(len(sys.argv), sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
