94ca0305a3f0a328bbbab9fd31093c33d8858bd7
bernaba123 <bernabasgetachew0946@gmail.com>
2021-04-29 12:56:35 +0000

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
-        print("{:d}: {:s}\n".format(len(sys.argv), sys.argv[1]), end="")
+        print("{:d}: {:s}".format(len(sys.argv), sys.argv[1]))
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}\n".format(i, sys.argv[i]), end="")
+        for i in range(len(sys.argv) - 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
