54e0bceb862372268922f658d97c6a77b37a94ab
bernaba123 <bernabasgetachew0946@gmail.com>
2021-05-04 13:10:24 +0000

    all

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            print("{}".format(my_string[i]), end="")
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+    return ''.join(c for c in my_string if c not in 'cC')
