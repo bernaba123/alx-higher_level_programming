56bb8bde5739f284deee2e9eebe9ada1eecfa5f1
bernaba123 <bernabasgetachew0946@gmail.com>
2021-05-04 12:01:56 +0000

    all

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            print("{:c}".format(my_string[i]))
+        else:
+            print("")
