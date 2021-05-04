cb54edbb52786af370b8052068124e49444b7ed7
bernaba123 <bernabasgetachew0946@gmail.com>
2021-05-04 14:01:05 +0000

    all

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,12 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" ")
         print("\n", end="")
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
