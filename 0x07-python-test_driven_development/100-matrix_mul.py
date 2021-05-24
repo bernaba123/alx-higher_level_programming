c1bca1c31dc133571e995c0d888d566056f5f60b
Bernabas Getachew <70795558+bernaba123@users.noreply.github.com>
2021-05-24 13:54:05 +0300

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,50 +1,55 @@
 #!/usr/bin/python3
+"""
+Contains the matrix_mul function
+"""
+
+
 def matrix_mul(m_a, m_b):
-    """ Function that multiplies two matrix
-    
-        Args:
-           m_a: matrix a
-           m_b: matrix b
-           
-        Returns:
-           new matrix with the multiplication or Errors
-    """
+    """Multiply two matrices(lists of lists of integers/floats)"""
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
+    l1 = len(m_a)
+    if l1 == 0:
+        raise ValueError("m_a can't be empty")
+    l2 = None
+    for i in m_a:
+        if type(i) is not list:
+            raise TypeError("m_a must be a list of lists")
+        if l2 is None:
+            l2 = len(i)
+            if l2 == 0:
+                raise ValueError("m_a can't be empty")
+        if l2 != len(i):
+            raise TypeError("each row of m_a must should be of the same size")
+        for j in i:
+            if type(j) is not int and type(j) is not float:
+                raise TypeError("m_a should contain only integers or floats")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-    m_a_files = len(m_a)
-    for row in m_a:
-        if type(row) is not list:
-            raise TypeError("m_a must be a list of lists")
-        m_a_rows = len(row)
-        if m_a_rows == 0:
-            raise TypeError("m_a can't be empty")
-            for index, j in enumerate(row[:]):
-                if type(j) is not int and type(j) is not float:
-                    raise TypeError("m_a should contain only integers or floats")
-
-    m_b_files = len(m_b)
-    for row in m_b:
-        if type(row) is not list:
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+    l3 = None
+    for i in m_b:
+        if type(i) is not list:
             raise TypeError("m_b must be a list of lists")
-        m_b_rows = len(row)
-        if m_b_rows == 0:
-            raise TypeError("m_b can't be empty")
-            for index, j in enumerate(row[:]):
-                if type(j) is not int and type(j) is not float:
-                    raise TypeError("m_b should contain only integers or floats")
-
-    if (m_a_files is not m_b_rows):
+        if l3 is None:
+            l3 = len(i)
+            if l3 == 0:
+                raise ValueError("m_b can't be empty")
+        if l3 != len(i):
+            raise TypeError("each row of m_b must should be of the same size")
+        for j in i:
+            if type(j) is not int and type(j) is not float:
+                raise TypeError("m_b should contain only integers or floats")
+    if l2 != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-
-    new_matrix = []
-    for i in range(len(m_a)):
-        mul_row = []
-        for j in range(len(m_b[0])):
-            item = 0
-            for k in range (len(m_b)):
-                item += m_a[i][k] * m_b[k][j]
-            mul_row.append(item)
-        new_matrix.append(mul_row)
-    return (new_matrix)
+    matrix = []
+    for i in range(l1):
+        l = []
+        for j in range(l3):
+            n = 0
+            for k in range(l2):
+                n += m_a[i][k] * m_b[k][j]
+            l.append(n)
+        matrix.append(l)
+    return matrix
