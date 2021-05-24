d4cbeb43e39551a36509af51a50dfbc8ac3eb0d5
Bernabas Getachew <70795558+bernaba123@users.noreply.github.com>
2021-05-24 13:22:28 +0300

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Module for multiplying 2 matrices using NumPy"""
-
-
+"""
+This module multiply 2 matricies using numpy module
+"""
 import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Function to multiply 2 matrices using NumPy"""
-    return (numpy.matmul(m_a, m_b))
+    """
+    multiply 2 matrix that is given
+    Args:
+        m_a: input first matrix
+        m_b: input second matrix
+    Returns:
+        return m_a * m_b
+    """
+    return numpy.matmul(m_a, m_b)
