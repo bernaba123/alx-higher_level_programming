be701e21ee5871628e60093766983667899907c7
bernaba123 <bernabasgetachew0946@gmail.com>
2021-06-01 08:28:09 +0000

    0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defining read_file function"""
+
+
+def read_file(filename=""):
+    """reads my_file_0.txt with utf-8"""
+    with open('my_file_0.txt', encoding='utf-8') as f:
+        """prints the text file"""
+        print(f.read(), end="")
