9d4c69120ed2038a1c776e8f9be3b2b9ecfc1674
bernaba123 <bernabasgetachew0946@gmail.com>
2021-06-01 10:10:32 +0000

    3-to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""defining to_json_string function"""
+
+
+def to_json_string(my_obj):
+    """returns json representation of an object """
+    return json.dumps(my_obj)
