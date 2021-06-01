759d5f885de8dccedcb153b38a059d37ec1a5f33
bernaba123 <bernabasgetachew0946@gmail.com>
2021-06-01 10:55:21 +0000

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """reads filename with utf-8"""
     with open(filename, "w", encoding='utf-8') as f:
-        """returns the number of characters"""
         return f.write(text)
