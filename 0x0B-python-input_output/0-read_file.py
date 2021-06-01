b8f04d6fc2b96137587826b51d6824d721add650
Bernabas Getachew <70795558+bernaba123@users.noreply.github.com>
2021-06-01 12:23:06 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,7 @@
 
 
 def read_file(filename=""):
-    """reads my_file_0.txt with utf-8"""
+    """reads filename with utf-8"""
     with open(filename, encoding='utf-8') as f:
         """prints the text file"""
         print(f.read(), end="")
