24703ee1b72ebf582e5caa24e929fadc11d7b6f4
Bernabas Getachew <70795558+bernaba123@users.noreply.github.com>
2021-08-03 19:45:02 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const fA = fs.readFileSync(process.argv[2], 'utf8');
-const fB = fs.readFileSync(process.argv[3], 'utf8');
-const answer = fA + fB;
-fs.writeFile(process.argv[4], answer, function (err) { if (err) throw err; });
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
